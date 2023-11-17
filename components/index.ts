99a93dc0002f24292411ffc6ece12e3d8ecfb660
Pratik Nath Tiwari <pratik@techalchemy.com>
2023-11-17 09:33:45 +0530

    Hero section finished

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-export {
-    Hero
-}
+export { Hero, CustomButton, Navbar, Footer };
